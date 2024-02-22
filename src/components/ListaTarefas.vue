bc74ff8b791f2d1bb9608c74d4e2bf3f406fd4e8
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-02-22 14:17:53 -0300

    :lipstick: style: lista sendo preenchida corretamente

--- src/components/ListaTarefas.vue
+++ src/components/ListaTarefas.vue
@@ -1,44 +1,31 @@
 <template>
     <Box>
-        <div class="columns">
-            <div class="column is-7">
-                {{ tarefa.descricao || 'Tarefa sem descrição' }}
-            </div>
-            <div class="column">
-                <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos" />
-            </div>
+      <div class="columns">
+        <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
+        <div class="column">
+          <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
         </div>
+      </div>
     </Box>
-</template>
-
-<script lang="ts">
-import { defineComponent, PropType } from 'vue';
-import Cronometro from './Cronometro.vue';
-import Box from './Box.vue';
-
-import ITarefa from '../interfaces/ITarefa';
-
-export default defineComponent({
-  name: 'ListaTarefas',
-  components: {
-    Cronometro,
-    Box
-  },
-  props: {
-    tarefa: {
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, PropType } from "vue";
+  import Cronometro from "./Cronometro.vue";
+  import ITarefa from '../interfaces/ITarefa'
+  import Box from './Box.vue'
+  
+  export default defineComponent({
+    name: "ListaTarefas",
+    components: {
+      Cronometro,
+      Box
+    },
+    props: {
+      tarefa: {
         type: Object as PropType<ITarefa>,
         required: true
+      }
     }
-  }
-});
-</script>
-
-<style scoped>
-    .columns {
-        align-items: center;
-    }
-    .column {
-        display: flex;
-        justify-content: center;
-    }
-</style>
+  });
+  </script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -1,38 +1,31 @@
 <template>
-        <div
-            class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <Cronometro :tempoEmSegundos="tempoEmSegundos" />
-          <BotoesCronometro 
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Cronometro :tempoEmSegundos="tempoEmSegundos" />
+    <BotoesCronometro 
           @iniciar="iniciar"
           @finalizar="finalizar"
           :cronometroRodando="cronometroRodando"
           />
-        </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Cronometro from "./Cronometro.vue";
-import BotoesCronometro from "./BotoesCronometro.vue";
+import Cronometro from './Cronometro.vue'
+import BotoesCronometro from './BotoesCronometro.vue'
 
 export default defineComponent({
-  name: "Formulario-vue",
-  emits: ['temporizadorFinalizado'],
+  name: "Temporizador-vue",
+  emits: ['aoTemporizadorFinalizado'],
   components: {
-      Cronometro,
-      BotoesCronometro
+    Cronometro,
+    BotoesCronometro
   },
   data () {
     return {
       tempoEmSegundos: 0,
       cronometro: 0,
       cronometroRodando: false
-    }
-  },
-  computed: {
-    tempoDecorrido () : string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
     }
   },
   methods: {
@@ -45,11 +38,11 @@ export default defineComponent({
       }, 1000)
     },
     finalizar () {
-        this.cronometroRodando = false
+      this.cronometroRodando = false
       clearInterval(this.cronometro)
-      this.$emit('temporizadorFinalizado', this.tempoEmSegundos) //Primeiro parametro é o nome do evento, o segundo é o dado que será enviado.
+      this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos)
       this.tempoEmSegundos = 0
-    },
+    }
   }
 });
 </script>
