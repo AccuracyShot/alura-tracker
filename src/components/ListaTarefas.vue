bfe68f205926520471188e20182fe32b501d7488
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2024-03-28 23:33:06 -0300

    :lipstick: style: adição do modal de edição

--- src/components/ListaTarefas.vue
+++ src/components/ListaTarefas.vue
@@ -1,6 +1,6 @@
 <template>
     <Box>
-      <div class="columns">
+      <div class="columns clicavel" @click="tarefaClicada">
         <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
         <div class="column is-4">
           {{ tarefa.projeto.nome }} 
@@ -20,6 +20,7 @@
   
   export default defineComponent({
     name: "ListaTarefas",
+    emits: ['aoTarefaClicada'],
     components: {
       Cronometro,
       Box
@@ -29,6 +30,18 @@
         type: Object as PropType<ITarefa>,
         required: true
       }
+    },
+    methods: {
+      tarefaClicada() {
+        this.$emit('aoTarefaClicada', this.tarefa)
+      }
     }
   });
   </script>
+
+<style scoped>
+.clicavel {
+    cursor: pointer;
+}
+
+</style>
--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,7 +1,32 @@
 <template>
         <Formulario @aoSalvarTarefa="salvarTarefa"/>
         <div class="lista">
-          <ListaTarefas v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+          <ListaTarefas v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa" />
+          <div class="modal" :class="{'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
+            <div class="modal-background"></div>
+              <div class="modal-card">
+                <header class="modal-card-head">
+                  <p class="modal-card-title">Editando tarefa</p>
+                  <button @click="fecharModal" class="delete" aria-label="close"></button>
+                </header>
+                <section class="modal-card-body">
+                  <div class="field">
+                    <label for="descricaoDaTarefa" class="label">
+                      Descrição da tarefa
+                    </label>
+                    <input type="text"
+                     class="input"
+                     v-model="tarefaSelecionada.descricao" placeholder="Descrição da tarefa">
+                  </div>
+                </section>
+                <footer class="modal-card-foot">
+                  <div class="buttons">
+                    <button class="button is-success">Salvar Alterações</button>
+                    <button @click="fecharModal" class="button">Cancelar</button>
+                  </div>
+                </footer>
+              </div>
+          </div>
           <Box v-if="listaEstaVazia">
             Você não está muito produtivo hoje, hein?
           </Box>
@@ -13,7 +38,7 @@
   import Formulario from '../components/Formulario.vue'
   import ListaTarefas from '../components/ListaTarefas.vue'
   import Box from '../components/Box.vue'
-  import { CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS } from '@/store/tipo-acoes';
+  import { CADASTRAR_TAREFA, OBTER_TAREFAS } from '@/store/tipo-acoes';
 import { useStore } from '@/store';
 import ITarefa from '@/interfaces/ITarefa';
   
@@ -24,6 +49,11 @@ import ITarefa from '@/interfaces/ITarefa';
       ListaTarefas,
       Box
     },
+    data () {
+      return {
+        tarefaSelecionada: null as ITarefa | null
+      }
+    },
     computed: {
       listaEstaVazia () : boolean {
         return this.tarefas.length == 0
@@ -33,6 +63,12 @@ import ITarefa from '@/interfaces/ITarefa';
       salvarTarefa (tarefa: ITarefa): void {
         this.store.dispatch(CADASTRAR_TAREFA, tarefa)
       },
+      selecionarTarefa (tarefa: ITarefa): void {
+        this.tarefaSelecionada = tarefa
+      },
+      fecharModal (): void {
+        this.tarefaSelecionada = null
+      }
     },
     setup () {
       const store = useStore();
